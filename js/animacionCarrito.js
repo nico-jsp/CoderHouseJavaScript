d6b44df15a5936d20d7f1bbc47a7aefe6b9739cf
nico-jsp <nicojsp@gmail.comgit config --listgit config --global user.email nicojsp@gmail.com>
2022-08-23 23:58:49 -0300

    Aplicando carrito con local storage. Si se carga la pagina, se recupera el carrito guardado

--- js/animacionCarrito.js
+++ js/animacionCarrito.js
@@ -2,7 +2,28 @@
 let menuApp = " \n a - agregar uno/otro curso \n q - quitar un curso \n 1 para pagar \n 0 para salir:"
 let nroIngresado = "";
 
+let cantidadCarritoWeb = 0;
+document.getElementById("carritoWeb").value = cantidadCarritoWeb;
+const btnVerCarrito = document.getElementById('verCarro');
+
+
 let cursosAInscribirse = [];
+if (localStorage.getItem("cursosAInscribirse")) {
+
+    cursosAInscribirse = JSON.parse(localStorage.getItem("cursosAInscribirse"));
+    cantidadCarritoWeb = cursosAInscribirse.length;
+    document.getElementById("carritoWeb").value = cantidadCarritoWeb;
+    if (cantidadCarritoWeb > 0) {
+
+        btnVerCarrito.innerHTML = `
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5Z"/>
+                      <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
+                      </svg>
+                      `;
+
+    }
+}
 let totalAPagar = 0;
 let carrito = [];
 let carritoWeb = [];
@@ -112,11 +133,11 @@ function imprimirCarrito() {
 //Agregar al carrito
 function agregarAlCarrito(nroIngresado) {
 
-    // cursosAInscribirse.push(nroIngresado);
+
     cursosAInscribirse.push(listaDeCursos[nroIngresado - 1]);
     totalAPagar = totalAPagar + listaDeCursos[nroIngresado - 1].precio;
     localStorage.setItem("cursosAInscribirse", JSON.stringify(cursosAInscribirse));
-    // console.log(localStorage.getItem("cursosAInscribirse"));
+
 
 }
 
@@ -130,7 +151,6 @@ function quitarDelCarrito(nroIngresado) {
         cantidadCarritoWeb -= 1;
         document.getElementById("carritoWeb").value = cantidadCarritoWeb;
         localStorage.setItem("cursosAInscribirse", JSON.stringify(cursosAInscribirse));
-        // console.log(localStorage.getItem("cursosAInscribirse"));
 
     } else {
         alert("El curso que se desea borrar no esta en el carrito");
@@ -140,22 +160,6 @@ function quitarDelCarrito(nroIngresado) {
 }
 
 
-
-// function accionar(nroIngresado, accion) {
-
-//     if (accion == "a") {
-//         agregarAlCarrito(nroIngresado);
-//         imprimirCarrito();
-//     }
-//     if (accion == "q") {
-//         quitarDelCarrito(nroIngresado);
-//         imprimirCarrito();
-
-//     }
-
-// }
-
-
 // Logica a seguir
 
 let listado = listarCursos();
@@ -163,13 +167,13 @@ let listado = listarCursos();
 
 // inicializamos el carrito
 
-let cantidadCarritoWeb = 0;
-document.getElementById("carritoWeb").value = cantidadCarritoWeb;
 
-const btnVerCarrito = document.getElementById('verCarro');
+
 const btnCerrarCarrito = document.getElementById('closeCarrito');
 const modalCarrito = document.getElementById('modal__container');
 const carritoHTML = document.getElementById('modal__body');
+const cursosListados = document.getElementById("workshops__list");
+renderizarProductos();
 
 
 // Funciones de botones
@@ -203,9 +207,18 @@ function verCarrito() {
     botonPagar.className = "botonPagar";
     botonPagar.innerText = "Finalizar Compra";
     carritoHTML.append(botonPagar);
-
+    // botonPagar.addEventListener("click", finalizarCompra());
 }
 
+function finalizarCompra() {
+
+    // Falta finalizar compra!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // for (const curso of cursosAInscribirse) {
+    //     quitarDelCarrito(curso.id);
+    // }
+    // localStorage.clear("cursosAInscribirse");
+
+}
 
 
 
@@ -218,11 +231,11 @@ function pressButton(i) {
     if (cantidadCarritoWeb > 0) {
         console.log(btnVerCarrito.innerHTML)
         btnVerCarrito.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5Z"/>
-  <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
-</svg>
-`;
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5Z"/>
+                      <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
+                      </svg>
+                      `;
 
     }
 
@@ -251,20 +264,20 @@ function removeCursoDeCarrito(i) {
 
 
 // Lista de Cursos
-const cursosListados = document.getElementsByClassName("workshops__list");
-for (const curso of listaDeCursos) {
-    let caracteristics = document.createElement("ul");
-    caracteristics.className = "caracteristica";
-    caracteristics.innerHTML = `
+function renderizarProductos() {
+    for (const curso of listaDeCursos) {
+        let caracteristics = document.createElement("ul");
+        caracteristics.className = "caracteristica";
+        caracteristics.innerHTML = `
         <li>Encuentros: ${curso.caracteristicas.numEncuentros}</li>
         <li>Horas: ${curso.caracteristicas.horasCatedra}</li>
         <li>Tipo de Encuentro: ${curso.caracteristicas.tipoEncuentro}</li>
         `;
 
 
-    const item = document.createElement("div");
-    item.className = "workshop";
-    item.innerHTML = `
+        const item = document.createElement("div");
+        item.className = "workshop";
+        item.innerHTML = `
         <h3>${curso.id} - ${curso.titulo}</h3>
         <p>
             (Taller para ${curso.publico} - USD ${curso.precio})
@@ -276,8 +289,16 @@ for (const curso of listaDeCursos) {
         
 
     `;
+        // href="" onclick='pressButton(${curso.id})
+        cursosListados.append(item);
 
-    cursosListados[0].append(item);
+
+    }
+    // Falta quitar el "href="" onclick='pressButton(${curso.id})" y ver por que cuando se carga se carga uno de cada uno tambien
+    // listaDeCursos.forEach(curso => {
+    //     //evento individual para cada boton
+    //     document.getElementById(`boton${curso.id}`).addEventListener("click", pressButton(curso.id));
+    // });
 
 }
 
