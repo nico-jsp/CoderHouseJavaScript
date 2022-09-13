2934eb46f195fef0e093a1616ce248e0e62d7ea7
nico-jsp <nicojsp@gmail.comgit config --listgit config --global user.email nicojsp@gmail.com>
2022-09-13 12:35:11 -0300

    Primer entrega final proyecto JavScript- carga archivo js

--- js/animacionCarrito.js
+++ js/animacionCarrito.js
@@ -10,11 +10,33 @@ let totalAPagar = 0;
 let carrito = [];
 let carritoWeb = [];
 
+const btnMetodoPago = document.getElementById('btnMetodoPago');
 const btnfinCompra = document.getElementById('finCompra');
 const btnCerrarCarrito = document.getElementById('closeCarrito');
 const modalCarrito = document.getElementById('modal__container');
+const modalMetodoPago = document.getElementById('modal__payment');
 const carritoHTML = document.getElementById('modal__body');
+const modalInputMetodoPago = document.getElementById('modal__payment__body');
 const cursosListados = document.getElementById("workshops__list");
+const tarjeta = document.querySelector('#tarjeta');
+const btnAbrirFormulario = document.getElementById('btn-abrir-formulario');
+const formularioTarjeta = document.getElementById('formulario-tarjeta');
+const numeroTarjeta = document.querySelector('#numero .numero');
+const nombreTarjeta = document.querySelector('#nombre .nombre');
+const logoMarca = document.getElementById('logo-marca');
+const firma = document.querySelector('#firma .firma p');
+const mesExpiracion = document.querySelector('#expirationDate .mes');
+const yearExpiracion = document.querySelector('#expirationDate .year');
+const ccv = document.querySelector('#tarjeta .ccv');
+
+// Rotamos la tarjeta para mostrar el frente.
+const mostrarFrente = () => {
+    if (tarjeta.classList.contains('active')) {
+        tarjeta.classList.remove('active');
+    }
+}
+
+
 // renderizarProductos();
 getJSON();
 
@@ -39,71 +61,6 @@ if (localStorage.getItem("cursosAInscribirse")) {
     }
 }
 
-
-
-// var listaDeCursos = [
-//     {
-//         id: 1,
-//         titulo: "Aspectos psicológicos en el crossfit I",
-//         descripcion: "",
-//         caracteristicas: {
-//             numEncuentros: 1,
-//             horasCatedra: 4,
-//             tipoEncuentro: ["Presencial"],
-//         },
-//         publico: "todos",
-//         precio: 50,
-//     },
-//     {
-//         id: 2,
-//         titulo: "Aspectos psicológicos en el crossfit II",
-//         descripcion: "",
-//         caracteristicas: {
-//             numEncuentros: 1,
-//             horasCatedra: 4,
-//             tipoEncuentro: ["Presencial"],
-//         },
-//         publico: "todos",
-//         precio: 80,
-//     },
-//     {
-//         id: 3,
-//         titulo: "Liderazgo en el crossfit",
-//         descripcion: "",
-//         caracteristicas: {
-//             numEncuentros: 1,
-//             horasCatedra: 5,
-//             tipoEncuentro: ["Presencial", "Online"],
-//         },
-//         publico: "coaches",
-//         precio: 90,
-//     },
-//     {
-//         id: 4,
-//         titulo: "La comunicacion en el box",
-//         descripcion: "",
-//         caracteristicas: {
-//             numEncuentros: 1,
-//             horasCatedra: 2,
-//             tipoEncuentro: ["Presencial", "Online"],
-//         },
-//         publico: "coaches",
-//         precio: 90,
-//     },
-//     {
-//         id: 5,
-//         titulo: "PSICO-STRENGTH: Entrenamiento mental para atletas",
-//         descripcion: "",
-//         caracteristicas: {
-//             numEncuentros: 4,
-//             horasCatedra: 8,
-//             tipoEncuentro: ["Presencial"],
-//         },
-//         publico: "atletas",
-//         precio: 90,
-//     }
-// ];
-
 // Funciones
 
 //Get JSON de cursos.json
@@ -120,26 +77,6 @@ async function getJSON() {
     renderizarProductos();
 }
 
-// Armar carrito
-// function imprimirCarrito() {
-//     let i = 0;
-//     let items = cursosAInscribirse.length;
-
-
-//     carrito = "";
-//     while (i < items) {
-
-//         carrito = carrito + "* ";
-//         carrito = carrito + " " + cursosAInscribirse[i].id + " - " + cursosAInscribirse[i].titulo;
-//         carrito = carrito + "\n";
-//         i++;
-
-//     };
-
-//     carrito = carrito + "\nTotal a pagar: " + totalAPagar;
-
-
-// }
 //Agregar al carrito
 function agregarAlCarrito(nroIngresado) {
 
@@ -153,8 +90,7 @@ function agregarAlCarrito(nroIngresado) {
 
 // Quitar del carrito
 function quitarDelCarrito(nroIngresado) {
-    // console.log(cursosAInscribirse);
-    // console.log(nroIngresado);
+
     let posicion = cursosAInscribirse.indexOf(listaDeCursos[nroIngresado - 1]);
     if (posicion != -1) {
         cursosAInscribirse.splice(posicion, 1);
@@ -170,24 +106,6 @@ function quitarDelCarrito(nroIngresado) {
 
 
 }
-
-
-// Logica a seguir
-
-// let listado = listarCursos();
-
-
-// inicializamos el carrito
-
-
-// const btnfinCompra = document.getElementById('finCompra');
-// const btnCerrarCarrito = document.getElementById('closeCarrito');
-// const modalCarrito = document.getElementById('modal__container');
-// const carritoHTML = document.getElementById('modal__body');
-// const cursosListados = document.getElementById("workshops__list");
-// // renderizarProductos();
-// getJSON();
-
 
 // Funciones de botones
 
@@ -221,13 +139,15 @@ function verCarrito() {
 
 
 }
-
+//Boton Comprar del curso "i"
 function pressButton(i) {
     agregarAlCarrito(i);
 
     carritoWeb.push(listaDeCursos[i - 1]);
+    //Sumar 1 a la cantidad de cursos en el carrito
     cantidadCarritoWeb += 1;
     document.getElementById("carritoWeb").value = cantidadCarritoWeb;
+    //Cambio el icono del carrito. Taza vacia - Taza con humito
     if (cantidadCarritoWeb > 0) {
         btnVerCarrito.innerHTML = `
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16">
@@ -245,19 +165,14 @@ function pressButton(i) {
 
 };
 
-function removeCurso(i) {
-    quitarDelCarrito(i);
-    // imprimirCarrito();
-
-
-}
+//Remover curso del carrito
 function removeCursoDeCarrito(i) {
     quitarDelCarrito(i);
     // imprimirCarrito();
     carritoHTML.innerHTML = "";
     verCarrito();
     if (cantidadCarritoWeb == 0) {
-        console.log(btnVerCarrito.innerHTML)
+        // console.log(btnVerCarrito.innerHTML)
         btnVerCarrito.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-fill" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M.11 3.187A.5.5 0 0 1 .5 3h13a.5.5 0 0 1 .488.608l-.22.991a3.001 3.001 0 0 1-1.3 5.854l-.132.59A2.5 2.5 0 0 1 9.896 13H4.104a2.5 2.5 0 0 1-2.44-1.958L.012 3.608a.5.5 0 0 1 .098-.42Zm12.574 6.288a2 2 0 0 0 .866-3.899l-.866 3.9Z"/>
       </svg>`;
@@ -267,7 +182,7 @@ function removeCursoDeCarrito(i) {
 }
 
 
-// Lista de Cursos
+// Lista de Cursos - renderizado
 function renderizarProductos() {
     for (const curso of listaDeCursos) {
         let caracteristics = document.createElement("ul");
@@ -306,7 +221,7 @@ function renderizarProductos() {
 
 }
 
-
+//Boton para mostrar el estado actual del carrito
 btnVerCarrito.addEventListener('click', () => {
 
 
@@ -315,45 +230,234 @@ btnVerCarrito.addEventListener('click', () => {
         modalCarrito.classList.add('show');;
     }
 
-    // const btnFinCompra = document.getElementById("botonPagar");
-    // btnFinCompra.addEventListener('onclick', finalizarCompra())
-
 });
 
+//Boton cerrar carrito de compra
 btnCerrarCarrito.addEventListener('click', () => {
     modalCarrito.classList.remove('show');
     carritoHTML.innerHTML = "";
 
 });
 
+//Boton fin de compra
 btnfinCompra.addEventListener('click', () => {
-    cursosAInscribirse = [];
-    localStorage.clear("cursosAInscribirse");
-    modalCarrito.classList.remove('show');
-    carritoHTML.innerHTML = "";
-    cantidadCarritoWeb = 0;
-    document.getElementById("carritoWeb").value = cantidadCarritoWeb;
-    btnVerCarrito.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-fill" viewBox="0 0 16 16">
+
+    if (((numeroTarjeta.innerText != '') && (numeroTarjeta.innerText.length == 19)) && (nombreTarjeta.innerText != '')
+        && (mesExpiracion.innerText != 'Mes')
+        && (yearExpiracion.innerText != 'Año')
+        && (ccv.innerText != '')) {
+        let total = 0;
+        for (const curso of cursosAInscribirse) {
+            total += curso.precio;
+        }
+
+        setTimeout(() => {
+
+            eventoPagar(true);
+
+            // Cierra timer
+        }, 30000);
+
+        cursosAInscribirse = [];
+        localStorage.clear("cursosAInscribirse");
+        modalCarrito.classList.remove('show');
+        carritoHTML.innerHTML = "";
+        cantidadCarritoWeb = 0;
+        document.getElementById("carritoWeb").value = cantidadCarritoWeb;
+        btnVerCarrito.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-fill" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M.11 3.187A.5.5 0 0 1 .5 3h13a.5.5 0 0 1 .488.608l-.22.991a3.001 3.001 0 0 1-1.3 5.854l-.132.59A2.5 2.5 0 0 1 9.896 13H4.104a2.5 2.5 0 0 1-2.44-1.958L.012 3.608a.5.5 0 0 1 .098-.42Zm12.574 6.288a2 2 0 0 0 .866-3.899l-.866 3.9Z"/>
       </svg>`;
 
-    Swal.fire(
-        "Ud ha realizado una compra",
-        "A disfrutar de los cursos!!!",
-        "success"
-    );
+        // Timer para procesar el pago
+        // console.log('aca llega antes de la funcion que carga')
+        console.log('aca llega antes de la PROMISE')
+
+
+
+
+        // Confirmacion de compra exitosa
+        Swal.fire(
+            "Ud ha realizado una compra",
+            `Su pago de USD${total} fue acreditado. A disfrutar de los cursos!!!`,
+            "success"
+        );
+
+
+    };
 })
 
-function finalizarCompra() {
 
-    // Falta finalizar compra!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // for (const curso of cursosAInscribirse) {
-    //     quitarDelCarrito(curso.id);
-    // // }
-    // cursosAInscribirse = [];
-    // localStorage.clear("cursosAInscribirse");
+// Funcion carga la venta y pasa los datos de la tarjeta para el pago
+// Solo para esperar una promesa y que paso un "tiempo"
+
+const eventoPagar = (info) => {
+
+    return new Promise((resolve, reject) => {
+        // setTimeout(() => {
+        if (info) {
+            let timerInterval
+            Swal.fire({
+                title: 'Procesando el pago',
+                html: 'Por favor espere.',
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log('I was closed by the timer')
+
+                }
+            })
+            return resolve('Promesa Resuelta')
+
+        } else {
+            return reject('Promesa Rechazada');
+        }
+        // }, 5000)
+        console.log('aca llega antes de la funcion que carga con la promise')
+
+    })
 
 }
+
+
+
+
+//Boton Metodo de Pago
+btnMetodoPago.addEventListener('click', () => {
+
+    if (!modalMetodoPago.classList.contains("show")) {
+        modalMetodoPago.classList.add('show');;
+    }
+
+})
+
+// Animacion tarjeta
+
+// Rotacion de tarjeta
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+})
+
+// Boton abrir formulario 
+btnAbrirFormulario.addEventListener('click', () => {
+    btnAbrirFormulario.classList.toggle('active');
+    formularioTarjeta.classList.toggle('active');
+})
+
+
+// Select del Mes
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formularioTarjeta.selectMes.appendChild(opcion);
+
+}
+
+// Select del año
+const yearActual = new Date().getFullYear();
+for (let i = yearActual; i <= yearActual + 8; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formularioTarjeta.selectYear.appendChild(opcion);
+
+}
+
+// Input numero de tarjeta
+formularioTarjeta.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formularioTarjeta.inputNumero.value = valorInput
+        // Eliminamos los espacios en Blanco
+        .replace(/\s/g, '')
+        // Elimina las letras
+        .replace(/\D/g, '')
+        // Agregamos un espacio cada 4 numeros
+        .replace(/([0-9]{4})/g, '$1 ')
+        // Elimina el ultimo espacio
+        .trim();
+
+
+    numeroTarjeta.textContent = valorInput;
+    if (valorInput == '') {
+        numeroTarjeta.textContent = '#### #### #### ####';
+        logoMarca.innerHTML = '';
+    }
+
+    if (valorInput[0] == 4) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.classList.toggle('img-card');
+        imagen.src = '../assets/img/card/logos/visa.png';
+        logoMarca.appendChild(imagen);
+    } else if (valorInput[0] == 5) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.classList.toggle('img-card');
+        imagen.src = '../assets/img/card/logos/mastercard.png';
+        logoMarca.appendChild(imagen);
+    }
+
+    // Rotamos la tarjeta cuando el usuario escribe
+    mostrarFrente();
+
+});
+
+// Input Nombre de Tarjeta
+formularioTarjeta.inputNombre.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formularioTarjeta.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+    nombreTarjeta.textContent = valorInput;
+    firma.textContent = valorInput;
+
+    if (valorInput == '') {
+        nombreTarjeta.textContent = 'Juan Perez';
+    }
+
+    mostrarFrente();
+});
+
+// Select de el mes
+formularioTarjeta.selectMes.addEventListener('change', (e) => {
+    mesExpiracion.textContent = e.target.value;
+    mostrarFrente();
+});
+
+// Select de el año
+formularioTarjeta.selectYear.addEventListener('change', (e) => {
+    yearExpiracion.textContent = e.target.value.slice(2);
+    mostrarFrente();
+});
+
+// Codigo CCV
+formularioTarjeta.inputCCV.addEventListener('keyup', () => {
+    if (!tarjeta.classList.contains('active')) {
+        tarjeta.classList.toggle('active');
+    }
+
+    formularioTarjeta.inputCCV.value = formularioTarjeta.inputCCV.value
+        // Eliminamos los espacios en Blanco
+        .replace(/\s/g, '')
+        // Elimina las letras
+        .replace(/\D/g, '');
+
+    ccv.textContent = formularioTarjeta.inputCCV.value;
+});
+
+
 
 
 
